391a2f6293b2e522ffa50dd232c7cc446431f0dd
Marius Žakevičius <marius.zakevicius@vinted.com>
2023-01-26 12:29:25 +0200

    Use SHA256 class directly

--- crypto_hash.rb
+++ crypto_hash.rb
@@ -5,6 +5,6 @@ class CryptoHash
   attr_reader :hex_digest
 
   def initialize(*args)
-    @hex_digest = Digest::SHA2.new(256).hexdigest [*args].map(&:to_s).sort.join(' ')
+    @hex_digest = Digest::SHA256.hexdigest [*args].map(&:to_s).sort.join(' ')
   end
 end
